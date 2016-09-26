82a8a423cda9cc55699119f12ba88d2c696ede59
durgesh-rane <drane@ncsu.edu>
2016-09-26 10:47:26 -0400

    Solved compile errors

--- mythread.c
+++ mythread.c
@@ -5,38 +5,38 @@
 
 #define STACKSIZE 8192
 
-Queue *readyQueue;
-Queue *blockedQueue;
+_Queue *readyQueue;
+_Queue *blockedQueue;
 _MyThread *currentThread;
 ucontext_t initContext;
 
 _MyThread *ThreadInit(void (*start_funct)(void *), void *args)
 {
 	_MyThread *initThread = (_MyThread *)malloc(sizeof(_MyThread));
-	getcontext(&(initThread->cxt));
-	(initThread->cxt).uc_link = NULL;
-	(newThread->cxt).uc_stack.ss_sp = malloc (STACKSIZE*8);
-	(newThread->cxt).uc_stack.ss_size = STACKSIZE;
+	getcontext(&(initThread->context));
+	(initThread->context).uc_link = NULL;
+	(initThread->context).uc_stack.ss_sp = malloc (STACKSIZE*8);
+	(initThread->context).uc_stack.ss_size = STACKSIZE;
 	initThread->blockedWith = NULL;
 	initThread->parent = NULL;
 	initThread->children = (_Queue *)malloc(sizeof(_Queue));
-	initializeQueue(initThread->children);
+	initQueue(initThread->children);
 		
-	makecontext(&(initThread->cxt), (void(*)())start_funct, 1, args);
+	makecontext(&(initThread->context), (void(*)())start_funct, 1, args);
 	
-	return newThread;
+	return initThread;
 }
 
 void MyThreadInit (void(*start_funct)(void *), void *args)
 {
-	getcontext(&initContext)
-	readyQueue = (Queue *)malloc(sizeof(Queue));
-	blockedQueue = (Queue *)malloc(sizeof(Queue));
-	initializeQueue(readyQueue);
-	initializeQueue(blockedQueue);
+	getcontext(&initContext);
+	readyQueue = (_Queue *)malloc(sizeof(_Queue));
+	blockedQueue = (_Queue *)malloc(sizeof(_Queue));
+	initQueue(readyQueue);
+	initQueue(blockedQueue);
 	
 	currentThread = ThreadInit(start_funct, args);
-	swapcontext(&initContext, &(currentThread->cxt));
+	swapcontext(&initContext, &(currentThread->context));
 }
 
 MyThread MyThreadCreate (void(*start_funct)(void *), void *args)
@@ -54,7 +54,7 @@ void MyThreadYield(void)
 	enqueue(readyQueue, yieldThread);
 	_MyThread *runThread = dequeue(readyQueue);
 	currentThread = runThread;
-	swapcontext(&(yieldThread->context),&(currentThred->context));	
+	swapcontext(&(yieldThread->context),&(currentThread->context));	
 }
 
 int MyThreadJoin(MyThread thread)
@@ -65,7 +65,7 @@ int MyThreadJoin(MyThread thread)
 		return -1;
 	if(joinThread->parent != currentThread)
 		return -1;
-	if(!isPresentInQueue(currentThread->children, joinThread)
+	if(!isPresentInQueue(currentThread->children, joinThread))
 		return 0;
 		
 	_MyThread *blockThread = currentThread;
@@ -87,14 +87,14 @@ int MyThreadJoin(MyThread thread)
 
 void MyThreadJoinAll(void)
 {
-	if(isQueueEmpty(currentThread->children)
+	if(isQueueEmpty(currentThread->children))
 		return;
 	_MyThread *blockThread = currentThread;
 	enqueue(blockedQueue, blockThread);
 	_MyThread *runThread = dequeue(readyQueue);
 	if(!runThread)
 	{
-		setcontext(&initcontext);
+		setcontext(&initContext);
 	}
 	else
 	{
@@ -110,20 +110,20 @@ void MyThreadExit(void)
 	if(parentThread)
 	{
 		removeFromQueue(parentThread->children, exitThread);
-		if(parentThread->blockedWith == exitThread || isQueueEmpty(parentThread->children)
+		if(parentThread->blockedWith == exitThread || isQueueEmpty(parentThread->children))
 		{
 			removeFromQueue(blockedQueue, parentThread);
 			enqueue(readyQueue, parentThread);
 		}
 	}
-	_QueueNode *exitChildNode = exitThread->chidlren->front;
+	_QueueNode *exitChildNode = exitThread->children->front;
 	while(exitChildNode)
 	{
 		exitChildNode->thread->parent = NULL;
 		exitChildNode = exitChildNode->next;
 	}
 
-	free((exitThread->cxt).uc_stack.ss_sp);	
+	free((exitThread->context).uc_stack.ss_sp);	
 	free(exitThread->blockedWith);
 	exitThread->blockedWith = NULL;
 	free(exitThread->parent);
@@ -150,7 +150,7 @@ MySemaphore MySemaphoreInit(int initialValue)
 	if(initialValue<0)
 		return NULL;
 	
-	_MySemaphore semaphore = (_MySemaphore *)malloc(sizeof(_MySemaphore));
+	_MySemaphore *semaphore = (_MySemaphore *)malloc(sizeof(_MySemaphore));
 	semaphore->value = initialValue;
 	semaphore->blockedQueue = (_Queue *)malloc(sizeof(_Queue));
 	initQueue(semaphore->blockedQueue);
@@ -180,7 +180,7 @@ void MySemaphoreWait(MySemaphore sem)
 
 void MySemaphoreSignal(MySemaphore sem)
 {
-	_MySemaphore *semaphore = (MySemaphore *)sem;
+	_MySemaphore *semaphore = (_MySemaphore *)sem;
 	semaphore->value = semaphore->value + 1;
 	if(semaphore->value > 0)
 		return;
@@ -193,14 +193,14 @@ void MySemaphoreSignal(MySemaphore sem)
 
 int MySemaphoreDestroy(MySemaphore sem)
 {
-	_MySemaphore *semaphore = (MySemaphore *)sem;
+	_MySemaphore *semaphore = (_MySemaphore *)sem;
 	if(!semaphore)
 		return -1;
 	if(!isQueueEmpty(semaphore->blockedQueue))
 		return -1;
 
-	free(semaphore->blockedQ);
-	semaphore->blockedQ = NULL;
+	free(semaphore->blockedQueue);
+	semaphore->blockedQueue = NULL;
 	free(semaphore);
 	semaphore = NULL;
 	return 0;
