a0b0f51e82cdd3fedf5f333d33a92bb31e8c6ce5
durgesh-rane <drane@ncsu.edu>
2016-09-27 03:04:22 -0400

    Update mythread.c

--- mythread.c
+++ mythread.c
@@ -2,7 +2,7 @@
 #include <ucontext.h>
 #include "myqueue.h"
 #include "mythread.h"
-
+#include <stdio.h>
 #define STACKSIZE 8192
 
 _Queue *readyQueue;
@@ -17,13 +17,15 @@ _MyThread *ThreadInit(void (*start_funct)(void *), void *args)
 	(initThread->context).uc_link = NULL;
 	(initThread->context).uc_stack.ss_sp = malloc (STACKSIZE*8);
 	(initThread->context).uc_stack.ss_size = STACKSIZE;
+	initThread->blockedWith = (_MyThread *)malloc(sizeof(_MyThread));
 	initThread->blockedWith = NULL;
+	initThread->parent = (_MyThread *)malloc(sizeof(_MyThread));
 	initThread->parent = NULL;
 	initThread->children = (_Queue *)malloc(sizeof(_Queue));
 	initQueue(initThread->children);
+	initThread->joinAll = 0;
 		
 	makecontext(&(initThread->context), (void(*)())start_funct, 1, args);
-	
 	return initThread;
 }
 
@@ -45,6 +47,7 @@ MyThread MyThreadCreate (void(*start_funct)(void *), void *args)
 	createThread->parent = currentThread;
 	enqueue(readyQueue, createThread);	
 	enqueue(currentThread->children, createThread);
+	//printf("Created thread: %d\n",createThread);
 	return (MyThread)createThread;	
 }
 
@@ -73,15 +76,15 @@ int MyThreadJoin(MyThread thread)
 	currentThread->blockedWith = joinThread;
 	enqueue(blockedQueue, blockThread);
 	_MyThread *runThread = dequeue(readyQueue);
-	if(!runThread)
-	{
-		setcontext(&initContext);
-	}
-	else
-	{
+	//if(!runThread)
+	//{
+	//	setcontext(&initContext);
+	//}
+	//else
+	//{
 		currentThread = runThread;
 		swapcontext(&(blockThread->context), &(currentThread->context));
-	}
+	//}
 	return 0;	
 }
 
@@ -90,57 +93,92 @@ void MyThreadJoinAll(void)
 	if(isQueueEmpty(currentThread->children))
 		return;
 	_MyThread *blockThread = currentThread;
+	blockThread->joinAll = 1;
 	enqueue(blockedQueue, blockThread);
 	_MyThread *runThread = dequeue(readyQueue);
-	if(!runThread)
-	{
-		setcontext(&initContext);
-	}
-	else
-	{
+	//if(!runThread)
+	//{
+	//	setcontext(&initContext);
+	//}
+	//else
+	//{
 		currentThread = runThread;
 		swapcontext(&(blockThread->context), &(currentThread->context));
+	//}
+}
+
+int NumberOfNodesInQueue(_Queue *q)
+{
+	int count = 0;
+	_QueueNode *queueNode = q->front;
+	while(queueNode)
+	{
+		//printf("Thread info: %d\n", queueNode->thread);
+		count = count+1;
+		queueNode = queueNode->next;
 	}
+	return count;
 }
 
 void MyThreadExit(void)
 {
+	int readyQueueCount = NumberOfNodesInQueue(readyQueue);
+	//printf("Ready Queue count %d\n",readyQueueCount);
+	//printf("Current thread: %d\n", currentThread);
+	//printf("in my thread exit\n");
 	_MyThread *exitThread = currentThread;
 	_MyThread *parentThread = exitThread->parent;
 	if(parentThread)
 	{
+		//printf("in if condition parentThread: %d\n",parentThread);
+		//if(removeFromQueue(parentThread->children, exitThread))
+		//	printf("Successfully removed from parent thread's children queue\n");
 		removeFromQueue(parentThread->children, exitThread);
-		if(parentThread->blockedWith == exitThread || isQueueEmpty(parentThread->children))
+		if(parentThread->blockedWith == exitThread || (parentThread->joinAll && isQueueEmpty(parentThread->children)) )
 		{
-			removeFromQueue(blockedQueue, parentThread);
-			enqueue(readyQueue, parentThread);
+		//	printf("in if condition blocked queue parentThread\n");
+			if(removeFromQueue(blockedQueue, parentThread))
+			{
+		//		printf("Successfully removed from blockedQueue\n");
+				enqueue(readyQueue, parentThread);
+			}
 		}
 	}
-	_QueueNode *exitChildNode = exitThread->children->front;
-	while(exitChildNode)
+	if(!isQueueEmpty(exitThread->children))
 	{
-		exitChildNode->thread->parent = NULL;
-		exitChildNode = exitChildNode->next;
+		_QueueNode *exitChildNode = exitThread->children->front;
+		while(exitChildNode)
+		{
+			//printf("in while loop to mark parent null\n");
+			_MyThread *childThread = exitChildNode->thread;
+			childThread->parent = NULL;
+			exitChildNode = exitChildNode->next;
+		}
 	}
-
-	free((exitThread->context).uc_stack.ss_sp);	
-	free(exitThread->blockedWith);
-	exitThread->blockedWith = NULL;
-	free(exitThread->parent);
+	//removeFromQueue(readyQueue, exitThread);
+	//removeFromQueue(blockedQueue, exitThread);
+	//free((exitThread->context).uc_stack.ss_sp);
+	exitThread->blockedWith = NULL;	
+	//free(exitThread->blockedWith);
 	exitThread->parent = NULL;
-	free(exitThread->children);
+	//free(exitThread->parent);
 	exitThread->children = NULL;
-	free(exitThread);
-	exitThread = NULL;
+	//free(exitThread->children);
+	exitThread= NULL;
+	//free(exitThread);
 	
 	_MyThread *runThread = dequeue(readyQueue);	
 	if(!runThread)
 	{
+		//printf("in set context to init\n");
 		setcontext(&initContext);
 	}
 	else
 	{
+		//printf("in set context to dequeued thread\n");
 		currentThread = runThread;
+		//printf("current thread %d\n",currentThread);
+		//printf("current thread context %d\n", &(currentThread->context));
 		setcontext(&(currentThread->context));			
 	}	
 }
@@ -200,8 +238,6 @@ int MySemaphoreDestroy(MySemaphore sem)
 		return -1;
 
 	free(semaphore->blockedQueue);
-	semaphore->blockedQueue = NULL;
 	free(semaphore);
-	semaphore = NULL;
 	return 0;
 }
